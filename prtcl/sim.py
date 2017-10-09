200ddaa61604ff9de2b4eb6337d4f82adfd09098
Matthias Hannig <matthias@hannig.cc>
2017-10-09 10:52:47 +0200

    foobar

--- prtcl/sim.py
+++ prtcl/sim.py
@@ -13,27 +13,31 @@ FLOOR = particle.Particle((0,0), fixed=True)
 def simulate_debug(space):
     space.reset()
     t0 = time.time()
+    i = 0
     while True:
         t = time.time() - t0
 
-        if t > 5:
+        if i % (1 * FPS) == 0:
+            space.add_particle(particle.Particle((0, 16)))
+
+        if i % 16 == 0:
             FLOOR.collides = False
+
+        if i % 18 == 0:
+            FLOOR.collides = True
+
 
         space.update()
         space.print_state()
         time.sleep(1.0/FPS)
 
+        i += 1
 
 
 if __name__ == "__main__":
     s = space.Space(9.81)
 
     s.add_particle(FLOOR)
-    s.add_particle(particle.Particle((0, 16)))
-    s.add_particle(particle.Particle((0, 17.2)))
-    s.add_particle(particle.Particle((0, 18.4)))
-    s.add_particle(particle.Particle((0, 19.6)))
-    s.add_particle(particle.Particle((0, 20.8)))
 
     simulate_debug(s)
 
